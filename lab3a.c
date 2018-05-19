7f203c1714fd7cdf95e6426a417057c14a9c8c05
Devika Karumathil Chaudhuri <devika@lnxsrv07.seas.ucla.edu>
2018-05-19 14:59:44 -0700

    added the header file

--- lab3a.c
+++ lab3a.c
@@ -1,9 +1,18 @@
 //Devika Chaudhuri
 //Benjamin Domae
 
+#include "ext2_fs.h"
 
 
+int imagefd;
 
+int Open(char * pathname, int flags, mode_t mode){
+  int fd = open(pathname, flags, mode);
+  if (fd == -1){
+    fprintf(stderr, "Open error:%s\n", strerror(errno));
+  }
+  return fd;
+}
 
 void indirectblocks(){
 }
@@ -31,5 +40,6 @@ void superblock(){
 
 int main (int argc, char *argv[]){
   //argv is the name of the image.
-
+  image fd = Open(argv[1], O_RDONLY);// open the file image
+}
   
