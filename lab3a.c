8c2d0d3b20d69da3e69b4df839f599fae3200af9
Devika Karumathil Chaudhuri <devika@lnxsrv01.seas.ucla.edu>
2018-05-19 14:14:13 -0700

    added the trivial image

---
+++ lab3a.c
@@ -0,0 +1,35 @@
+//Devika Chaudhuri
+//Benjamin Domae
+
+
+
+
+
+void indirectblocks(){
+}
+
+void directory (){
+}
+
+void Inode(){
+}
+
+void freeInode(){
+}
+
+void freeblock(){
+}
+
+
+void group(){
+}
+
+
+void superblock(){
+}
+
+
+int main (int argc, char *argv[]){
+  //argv is the name of the image.
+
+  
