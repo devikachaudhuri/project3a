6cfab4727db13b1c4f160024ae9984b0284db428
Devika Karumathil Chaudhuri <devika@lnxsrv09.seas.ucla.edu>
2018-05-19 15:55:43 -0700

    superblock is done

---
+++ Makefile
@@ -0,0 +1,2 @@
+lab3a: lab3a.c
+	gcc -std=gnu11 -Wall -Wextra lab3a.c -o lab3a
--- lab3a.c
+++ lab3a.c
@@ -1,18 +1,35 @@
 //Devika Chaudhuri
 //Benjamin Domae
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <stdint.h>
 #include "ext2_fs.h"
 
-
 int imagefd;
+struct ext2_super_block sblock;
 
-int Open(char * pathname, int flags, mode_t mode){
-  int fd = open(pathname, flags, mode);
+int Open(char * pathname, int flags){
+  int fd = open(pathname, flags);
   if (fd == -1){
     fprintf(stderr, "Open error:%s\n", strerror(errno));
   }
   return fd;
 }
+
+ssize_t Pread(int fd, void *buf, size_t count, off_t offset){
+  ssize_t p = pread(fd, buf, count, offset);
+  if (p == -1){
+    fprintf(stderr, "pread error:%s\n", strerror(errno));
+  }
+  return p;
+}
+
 
 void indirectblocks(){
 }
@@ -35,11 +52,16 @@ void group(){
 
 
 void superblock(){
+  //The superblock is always located at byte offset 1024 from the beginning of the file, block device or partition formatted with Ext2 and later variants (Ext3, Ext4).
+  Pread(imagefd, &sblock, sizeof(struct ext2_super_block), 1024);
+  unsigned int block_size = 1024 << sblock.s_log_block_size;
+  printf("SUPERBLOCK,%u,%u,%u,%u,%u,%u,%u\n", sblock.s_blocks_count, sblock.s_inodes_count, block_size, sblock.s_inode_size, sblock.s_blocks_per_group, sblock.s_inodes_per_group, sblock.s_first_ino);  
 }
 
 
 int main (int argc, char *argv[]){
   //argv is the name of the image.
-  image fd = Open(argv[1], O_RDONLY);// open the file image
+  imagefd = Open(argv[1], O_RDONLY);// open the file image
+  superblock();
 }
   
