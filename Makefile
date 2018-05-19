55f5cbf638f5dd7c5447314e4192444f3e7edef8
Devika Karumathil Chaudhuri <devika@lnxsrv09.seas.ucla.edu>
2018-05-19 16:47:46 -0700

    added the groups functionality with one error

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 lab3a: lab3a.c
 	gcc -std=gnu11 -Wall -Wextra lab3a.c -o lab3a
+
+clean:
+	rm lab3a
--- lab3a.c
+++ lab3a.c
@@ -10,9 +10,11 @@
 #include <errno.h>
 #include <stdint.h>
 #include "ext2_fs.h"
+#include <stdlib.h>
 
 int imagefd;
 struct ext2_super_block sblock;
+struct ext2_group_desc *gdescriptors;
 
 int Open(char * pathname, int flags){
   int fd = open(pathname, flags);
@@ -48,6 +50,25 @@ void freeblock(){
 
 
 void group(){
+  int number_of_groups = 1 + (sblock.s_blocks_count - 1)/sblock.s_blocks_per_group;
+  printf("number of groups %d\n", number_of_groups);
+//(totalnumber of blocks -1)/blocks per group
+  int block_size = 1024 << sblock.s_log_block_size;
+  //first group is at the offset 1024 + block_size
+  //second group is at the offset 1024 + block_size + (block_size * blocks_per_group)
+  //third group is at the offset 1024 + block_size+ 2(block_size * blockes_per_group)
+  gdescriptors = (struct ext2_group_desc*) malloc(number_of_groups * sizeof(struct ext2_group_desc));
+  
+  for (int i = 0; i< number_of_groups; i++){
+    int offset = 1024 + block_size + (block_size * i * sblock.s_blocks_per_group);
+    Pread(imagefd, &gdescriptors[0], sizeof(struct ext2_group_desc), offset);
+
+    unsigned int total_blocks = sblock.s_blocks_per_group; //TODO may have more...
+    unsigned int total_inodes = sblock.s_inodes_per_group;
+    
+    printf("GROUP,%i,%u,%u,%u,%u,%u,%u,%u\n", i,total_blocks, total_inodes, gdescriptors[i].bg_free_blocks_count, gdescriptors[i].bg_free_inodes_count, gdescriptors[i].bg_block_bitmap, gdescriptors[i].bg_inode_bitmap, gdescriptors[i].bg_inode_table);
+  }
+
 }
 
 
@@ -63,5 +84,6 @@ int main (int argc, char *argv[]){
   //argv is the name of the image.
   imagefd = Open(argv[1], O_RDONLY);// open the file image
   superblock();
+  group();
 }
   
